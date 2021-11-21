3c1225b80d05fb94c70b69a05d5a3f057ba4985e
Georgi Spasov <gvspasov@gmail.com>
2021-11-21 21:12:55 +0200

    Remove default todos in stores

--- src/stores.ts
+++ src/stores.ts
@@ -1,7 +1,3 @@
 import { writable } from "svelte/store";
 
-export const TodosStore = writable([
-  { id: 1, description: "do the londering", done: true },
-  { id: 2, description: "clean the dishes", done: false },
-  { id: 3, description: "cook diner", done: false },
-]);
+export const TodosStore = writable([]);
